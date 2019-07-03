902ae0c2099f2fa48fb4bd2667f24173e5502187
fiveham <fiveham@users.noreply.github.com>
2019-07-03 15:18:27 -0400

    refactor like crazy

--- stokes.py
+++ stokes.py
@@ -1,120 +1,112 @@
-#Given several adjacent districts in terms of their boundaries, find the
-#boundary of the union of the districts.
-#
-#As long as all the boundaries share the same winding scheme, adding the
-#directed edges together should cause the oppositely-oriented edges of adjacent
-#polygons in the interior to negate one another while leaving the unopposed
-#edges of the outer boundary(ies) intact.
-#
-#A winding scheme is either of the following: 1. that outer boundaries' vertices
-#build up clockwise and inner boundaries' vertices build up counterclockwise, or
-#2. that outer boundaries' vertices build up counterclockwise and inner
-#boundaries' vertices build up clockwise.
-#
-#The script is named after Stokes's theorem, in which the net curl in a 2D
-#surface region (the integral of the curl-density at each point in the region)
-#can be summarily calculated by integrating a slightly different
-#curl/curl-density term over the 1D border of the 2D region in question.
-#Basically, curly things laid flat and pushed right up against each other
-#naturally push the curliness out to the edge. So, by just tracking where the
-#curliness in a region loaded with polygons is (even though this is discrete
-#curliness) and isn't, we can identify the outer edge(s) and ignore the inner
-#ones.
-#
-#Relevant SMBC: https://www.smbc-comics.com/comic/2014-02-24
+"""A script to find the net outer boundary or boundaries of a collection of 
+abutting polygons.
 
-def _proper(edge):
-    a = edge[0]
-    b = edge[1]
-    if a[0] < b[0]: #proper
-        return (edge, 1)
-    elif a[0] > b[0]: #improper
-        return ((edge[1], edge[0]), -1)
-    elif a[1] < b[1]: #proper
-        return (edge, 1)
-    elif a[1] > b[1]: #improper
-        return ((edge[1], edge[0]), -1)
+The name "stokes" is taken from the Stokes Theorem 
+(https://en.wikipedia.org/wiki/Stokes%27_theorem).
+
+It is difficult to explain why that association makes sense. See the 
+2014-02-24 edition of the webcomic Saturday Morning Breakfast Cereal
+(https://www.smbc-comics.com/comic/2014-02-24) for an explanation of 
+why it is difficult to explain."""
+
+def _orientation(side):
+    a, b = side
+    if a < b:
+        return 1
+    elif a > b:
+        return -1
     else:
-        raise Exception("edge links a point with itself")
+        raise ValueError('side connects a vertex to itself: ' + str(side))
 
-def _sides(polygon):
-    assert polygon[0] == polygon[-1]
-    result = []
-    for i in range(1, len(polygon)):
-        side = (polygon[i-1], polygon[i])
-        yield side
-
-def _vital_dicts(polygons):
-    verts_to_edges = {}
-    edges_proper_orients = {}
-    for polygon in polygons:
-        for edge in _sides(polygon):
-            edge_proper, orient = _proper(edge)
-
-            #edges that occur twice in two different polygons will end up
-            #overlapping in opposite directions, resulting in a net orientation
-            #of 0, whereas unique edges end up with a net orientation not
-            #equal to zero.
-            if edge_proper in edges_proper_orients:
-                orient += edges_proper_orients[edge_proper]
-            edges_proper_orients[edge_proper] = orient
-            
-            for point in edge:
-                if point in verts_to_edges:
-                    verts_to_edges[point].add(edge_proper)
-                else:
-                    verts_to_edges[point] = {edge_proper}
-    return (verts_to_edges, edges_proper_orients)
-
-def _vulgar(edge, orient):
-    if orient == 1:
-        return edge
-    elif orient == -1:
-        return (edge[1], edge[0])
-    else:
-        raise Exception("unexpected net edge orientation: " + orient)
-
-class SeedException(Exception):
-    pass
-
-def _seed_edge(edges_proper_orients, used_edges):
-    for edge, orient in edges_proper_orients.items():
-        if orient != 0 and edge not in used_edges:
-            return _vulgar(edge, orient)
-    raise SeedException()
-        
-def _next_edge(edges_proper, current_edge, edges_proper_orients):
-    for edge in edges_proper:
-        if edge != _proper(current_edge)[0] and edges_proper_orients[edge] != 0:
-            return _vulgar(edge, edges_proper_orients[edge])
-
-#Each polygon vertex list's first vertex should equal that list's last vertex
-def stokes(polygons):
-    if len(polygons) == 0:
-        return []
-    if len(polygons) == 1:
-        return [list(polygons[0])]
-
-    net_boundaries = []
+class SideMinder:
+    """A class to keep track of directed edges (polygon sides) being added
+    together."""
     
-    verts_to_edges, edges_proper_orients = _vital_dicts(polygons)
-    used_edges = set()
+    def __init__(self):
+        self.em = {}
+    
+    def add(self, side):
+        verts = frozenset(side)
+        new_net_orientation = self.em.get(verts, 0) + _orientation(side)
+        if new_net_orientation == 0:
+            del self.em[verts]
+        elif abs(new_net_orientation) == 1:
+            self.em[verts] = new_net_orientation
+        else:
+            raise ValueError('net orientation outside allowed range')
+    
+    def net_sides(self):
+        nets = set()
+        for fs, ori in self.em.items():
+            x,y = fs
+            alphab = (x,y)
+            baphla = (y,x)
+            if _orientation(alphab) == ori:
+                nets.add(alphab)
+            elif _orientation(baphla) == ori:
+                nets.add(baphla)
+            else:
+                raise ValueError('impossible orientation situation')
+        return nets
+
+def _cross_product(de1, de2):
+    """To be used with directed edges defined by their endpoints"""
+    va1,vb1 = de1
+    va2,vb2 = de2
+    
+    v1 = vb1[0] - va1[0], vb1[1] - va1[1]
+    v2 = vb2[0] - va2[0], vb2[1] - va2[1]
+    
+    return v1[0]*v2[1] - v1[1]*v2[0]
+
+def _next_side(side, vertex_to_sides, remaining_net_sides):
+    nexts = [a
+             for a in vertex_to_sides[side[1]]['next']
+             if a in remaining_net_sides]
+    if len(nexts) != 1:
+        #dirty hack designed to work if original outer bounds curl ccw
+        nexts.sort(key=(lambda x : _cross_product(side, x)))
+    
+    next_side = nexts[0]
+
+    remaining_net_sides.remove(next_side)
+    return next_side
+
+def stokes(polygons):
+    
+    side_adder = SideMinder()
+    for polygon in polygons:
+        for i in range(1, len(polygon)):
+            side_adder.add(polygon[i-1:i+1])
+    del polygons
+    
+    net_sides = side_adder.net_sides()
+    del side_adder
+    
+    vertex_to_sides = {}
+    for side in net_sides:
+        a, b = side
+        for c in side:
+            if c not in vertex_to_sides:
+                vertex_to_sides[c] = {'next':[], 'prev':[]}
+        vertex_to_sides[a]['next'].append(side)
+        vertex_to_sides[b]['prev'].append(side)
+    for v,d in vertex_to_sides.items():
+        assert d['next'], [v,d]
+        assert d['prev'], [v,d]
+    
+    net_boundaries = []
     while True:
         try:
-            current_edge = _seed_edge(edges_proper_orients, used_edges)
-            vertices = [current_edge[0], current_edge[1]]
-            used_edges.add(_proper(current_edge)[0])
-            while vertices[-1] != vertices[0]:
-                next_edge = _next_edge(
-                        verts_to_edges[vertices[-1]],
-                        current_edge,
-                        edges_proper_orients)
-                vertices.append(next_edge[1])
-                used_edges.add(_proper(next_edge)[0])
-                current_edge = next_edge
-            net_boundaries.append(vertices)
-        except SeedException:
-            break
-
-    net_boundaries.sort(key=len)
+            seed = next(iter(net_sides))
+        except StopIteration:
+            break #out of while-loop
+        else:
+            polygon = list(seed)
+            next_side = _next_side(seed, vertex_to_sides, net_sides)
+            while next_side != seed:
+                polygon.append(next_side[1])
+                next_side = _next_side(next_side, vertex_to_sides, net_sides)
+            net_boundaries.append(polygon)
+    
     return net_boundaries
