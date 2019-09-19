423ad149e479d11c7f7fb9000c2b8b275ff8c64f
fiveham <fiveham@users.noreply.github.com>
2019-09-19 10:57:55 -0400

    add scale param on fuzzy, crashfixes, bugfixes
    fix NameErrors
    add scale param to fuzzy
    reverse inner bounds for stokes
    use .items() when needed
    remove cells for innerboundaries more efficiently

--- neighboring.py
+++ neighboring.py
@@ -16,17 +16,17 @@ def get_probe_points(side, probe_radius):
         return (xm, ym - probe_radius), (xm, ym + probe_radius)
     else:
         m = (x1 - x2) / (y2 - y1) #slope of line ortho to `side`
-        x = ((1 + m**2) ** -0.5) * rad
+        x = ((1 + m**2) ** -0.5) * probe_radius
         return (xm + x, ym + m*x), (xm - x, ym - m*x)
 
-def fuzzy(shapes, probe_factor=1000):
+def fuzzy(shapes, probe_factor=1000, scale=16):
     
     #Isolate the non-seamless boundaries
     from stokes import stokes
     boundaries = []
     for shape in shapes:
-        boundaries.extend(shape.outers)
-        boundaries.extend(shape.inners)
+        boundaries.extend(list(o) for o in shape.outers)
+        boundaries.extend(list(reversed(list(i))) for i in shape.inners)
     stoked = stokes(boundaries)
     
     #Determine the distance off to either side of each stokes-surviving
@@ -45,20 +45,24 @@ def fuzzy(shapes, probe_factor=1000):
     import spindex
     cell_to_shapes = {}
     for shape in shapes:
+
+        #gather cells
         cells = set()
         for outer in shape.outers:
-            cells.update(spindex.get_cells_2d(outer))
+            cells.update(spindex.get_cells_2d(outer, scale=scale))
         for inner in shape.inners:
-            cells -= spindex.get_cells_2d(inner)
-        for inner in shape.inners:
-            cells.update(spindex.get_cells_1d(inner))
+            d1 = spindex.get_cells_1d(inner, scale=scale)
+            d2 = spindex.get_cells_2d(inner, scale=scale, boundary_cells=d1)
+            cells -= (d2 - d1)
+
+        #add mapping from each cell to the current shape
         for cell in cells:
             if cell not in cell_to_shapes:
                 cell_to_shapes[cell] = {}
             cell_to_shapes[cell][shape.info] = shape
     else:
         cell_to_shapes = {cell:list(shape_dict.values())
-                          for cell,shape_dict in cell_to_shapes}
+                          for cell, shape_dict in cell_to_shapes.items()}
     
     #start building the graph with the vertices (as ints)
     graph = set(range(len(shapes)))
@@ -74,10 +78,10 @@ def fuzzy(shapes, probe_factor=1000):
             side = boundary[i-1:i+1]
             shapes1, shapes2 = ({s.info
                                  for s
-                                 in cell_to_shapes[spindex.get_cell(probe1)]
+                                 in cell_to_shapes[spindex.get_cell(point)]
                                  if point in s}
-                                for point in probe_points(side,
-                                                          probe_radius))
+                                for point in get_probe_points(side,
+                                                              probe_radius))
             
             both = shapes1 & shapes2
             shapes1 -= both
@@ -94,14 +98,14 @@ def seamless(shapes):
     
     #build a mapping from each polygon-side to a set of the polygons that
     #have that side (as int indices)
-    side_to_shape = {}
+    side_to_shapes = {}
     for shape in shapes:
         for side in shape.sides:
             key = frozenset(side)
             try:
-                extant = side_to_shape[key]
+                extant = side_to_shapes[key]
             except KeyError:
-                side_to_shape[key] = {shape.info}
+                side_to_shapes[key] = {shape.info}
             else:
                 extant.add(shape.info)
     
