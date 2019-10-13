c094a9a29e9ca1df6617c6a42af1858bacbe3144
fiveham <fiveham@users.noreply.github.com>
2019-10-13 02:21:52 -0400

    move cross_sign into

--- geometry.py
+++ geometry.py
@@ -5,3 +5,9 @@ def dist2(p1, p2):
 
 def dist(p1, p2):
     return dist2(p1, p2) ** 0.5
+
+def cross_sign(a, b, c):
+    """Calculate the cross product of the vector from `a` to `b` times the
+       vector from `a` to `c`. Return the sign of the z component."""
+    x = (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+    return -1 if x < 0 else 1 if x > 0 else 0
